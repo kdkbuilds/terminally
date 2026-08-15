2c495290e92c6357fb81c7cdf797deb5d73a1777
kdkbuilds <heyitskdk@gmail.com>
2026-08-15 17:35:10 +0530

    model | Task > exposed getDescription method for jackson library

--- src/main/java/org/kdkbuilds/model/Task.java
+++ src/main/java/org/kdkbuilds/model/Task.java
@@ -30,6 +30,10 @@ public final class Task {
         return id;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     @Override
     public String toString() {
         return id + " : " + description;
